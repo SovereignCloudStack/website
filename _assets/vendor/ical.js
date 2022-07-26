5893c584a0d55ad750e7805ff3bda2f2d17f0557
Eduard Itrich <eduard@itrich.net>
2022-07-26 10:03:06 +0200

    Use npm to manage external modules (#351)
    * Switch to npm to manage JS components.
    Signed-off-by: Eduard Itrich <eduard@itrich.net>

---
+++ _assets/vendor/ical.js
@@ -0,0 +1,1 @@
+../../node_modules/ical.js/build/
---
+++ node_modules/@fullcalendar/icalendar/main.d.ts
@@ -0,0 +1,6 @@
+import * as _fullcalendar_common from '@fullcalendar/common';
+
+declare const _default: _fullcalendar_common.PluginDef;
+
+
+export default _default;
